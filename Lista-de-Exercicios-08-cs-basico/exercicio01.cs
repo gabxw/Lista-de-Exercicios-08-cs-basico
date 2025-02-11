a98dcf7908ee9d9af7934776582238ea2cc2d07a
Gabriel <gabriel.jmaciel13@gmail.com>
2025-02-10 21:56:06 -0300

    Adicionando mais exercicios.

---
+++ Lista-de-Exercicios-08-cs-basico/exercicio01.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_de_Exercicios_08_cs_basico
+{
+    internal class Exercicio01
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                var pares = i % 2 == 0; // Verifica se o número é par
+                if (pares)
+                {
+                    Console.Write(i + " "); // Imprime o número par
+                }
+            }
+        }
+    }
+}
